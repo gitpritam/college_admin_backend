efc2e54bcfcd8d3ea9425176fcb8d3464e7cdc78
gitpritam <pritammajhi10@gmail.com>
2025-08-06 17:44:25 +0530

    fix: change types of posted by in event schema and interface

--- src/@types/interface/schema/event.interface.ts
+++ src/@types/interface/schema/event.interface.ts
@@ -1,11 +1,13 @@
+import { ObjectId } from "mongoose";
+
 export interface IEvent {
-    event_id: string;
-    title: string;
-    description: string;
-    start_date: Date;
-    end_date: Date;
-    start_time: string;
-    end_time: string;
-    venue: string;
-    posted_by: string;
+  event_id: string;
+  title: string;
+  description: string;
+  start_date: Date;
+  end_date: Date;
+  start_time: string;
+  end_time?: string;
+  venue: string;
+  posted_by: ObjectId;
 }
--- src/models/event.model.ts
+++ src/models/event.model.ts
@@ -1,56 +1,56 @@
-import mongoose, {type Date} from "mongoose";
+import mongoose, { type Date } from "mongoose";
 import type { IEvent } from "../@types/interface/schema/event.interface";
 
 const timeRegex = /^([01]\d|2[0-3]):([0-5]\d)$/;
 const EventSchema = new mongoose.Schema<IEvent>(
-    {
-        event_id: {
-            type: String,
-            unique: true,    
-        },
-        title: {
-            type: String,
-            minlength: [3, "Title should be minimun 3 characters"],
-            maxlength: [100, "Title should be maximum 100 characters"],
-            required: true,
-            trim: true,
-        },
-        description: {
-            type: String,
-            required: true,
-            minlength: [3, "Description should be minimun 3 characters"],
-            maxlength: [100, "Description  should be maximum 100 characters"],
-            trim: true,
-        },
-        start_date: {
-            type: Date,
-            required: true,
-        },
-        end_date: {
-            type: Date,
-            required: true,
-        },
-        start_time: {
-            type: String,
-            required: true,
-            match: [timeRegex , "Time must be in HH:MM format"],
-        },
-        end_time: {
-            type: String,
-            match: [timeRegex, "Time must be in HH:MM format"],
-        },
-        venue: {
-            type: String,
-            required: true,
-            minlength: [3, "venue should be minimun 3 characters"],
-            maxlength: [100, "Venue should be maximum 100 characters"],
-        },
-        posted_by: {
-            type: String,
-            required: true,
-        }
+  {
+    event_id: {
+      type: String,
+      unique: true,
     },
-    {timestamps: true }
+    title: {
+      type: String,
+      minlength: [3, "Title should be minimun 3 characters"],
+      maxlength: [100, "Title should be maximum 100 characters"],
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      minlength: [3, "Description should be minimun 3 characters"],
+      maxlength: [250, "Description  should be maximum 250 characters"],
+      trim: true,
+    },
+    start_date: {
+      type: Date,
+      required: true,
+    },
+    end_date: {
+      type: Date,
+      required: true,
+    },
+    start_time: {
+      type: String,
+      required: true,
+      match: [timeRegex, "Time must be in HH:MM format"],
+    },
+    end_time: {
+      type: String,
+      match: [timeRegex, "Time must be in HH:MM format"],
+    },
+    venue: {
+      type: String,
+      required: true,
+      minlength: [3, "venue should be minimun 3 characters"],
+      maxlength: [100, "Venue should be maximum 100 characters"],
+    },
+    posted_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "faculties",
+    },
+  },
+  { timestamps: true }
 );
 const EventModel = mongoose.model<IEvent>("Events", EventSchema);
 export default EventModel;
