ae593f4dd6d2abf377adaffcfff6b18e7450638b
gitpritam <pritammajhi10@gmail.com>
2025-08-06 17:52:26 +0530

    fix: uploaded_by and visibility types changed

--- src/@types/interface/schema/resources.interface.ts
+++ src/@types/interface/schema/resources.interface.ts
@@ -1,10 +1,12 @@
-export interface IResource{
-    title: string;
-    uploaded_by: string;
-    file_type: string;
-    department: string;
-    course: string;
-    semester: string;
-    tags: string;
-    visibility: string;
+import { ObjectId } from "mongoose";
+
+export interface IResource {
+  title: string;
+  uploaded_by: ObjectId;
+  file_type: string;
+  department: string;
+  course: string;
+  semester?: string;
+  tags?: string[];
+  visibility?: boolean;
 }
--- src/models/resources.model.ts
+++ src/models/resources.model.ts
@@ -1,47 +1,46 @@
-import mongoose, {type Date} from "mongoose";
+import mongoose from "mongoose";
 import type { IResource } from "../@types/interface/schema/resources.interface";
 const ResourceSchema = new mongoose.Schema<IResource>(
-    {
-        title: {
-            type: String,
-            required: true,
-            minlength: [3, "Title should be minimun 3 characters"],
-            maxlength: [100, "Title should be maximum 100 characters"],
-            trim: true,
-        },
-        uploaded_by: {
-            type: String,
-            required: true,
-        },
-        file_type : {
-            type: String,
-            required: true,
-        },
-        department: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        course: {
-            type: String,
-            required: true,
-        },
-        semester: {
-            type: String,
-            required: true,
-        },
-        tags: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        visibility: {
-            type: String,
-            required: true,
-            trim: true,
-        }
+  {
+    title: {
+      type: String,
+      required: true,
+      minlength: [3, "Title should be minimun 3 characters"],
+      maxlength: [100, "Title should be maximum 100 characters"],
+      trim: true,
     },
-    {timestamps: true}
+    uploaded_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "faculties",
+      required: true,
+    },
+    file_type: {
+      type: String,
+      required: true,
+    },
+    department: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    course: {
+      type: String,
+      required: true,
+    },
+    semester: {
+      type: String,
+    },
+    tags: {
+      type: [String],
+      trim: true,
+    },
+    visibility: {
+      type: Boolean,
+      trim: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
 );
-const ResourceModel = mongoose.model<IResource>("Resources", ResourceSchema);
+const ResourceModel = mongoose.model<IResource>("resources", ResourceSchema);
 export default ResourceModel;
