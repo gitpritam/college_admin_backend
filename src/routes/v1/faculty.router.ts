b07ee26de067fd4841873ea692dcbf4457e874b6
gitpritam <pritammajhi10@gmail.com>
2025-08-10 21:36:00 +0530

    feat: get all faculty, get single faculty controller added

--- src/routes/v1/faculty.router.ts
+++ src/routes/v1/faculty.router.ts
@@ -1,12 +1,20 @@
 import { Router } from "express";
 import multer from "multer";
-import { createFacultyController } from "../../controllers/v1/faculty";
+import {
+  createFacultyController,
+  getAllFacultyController,
+  getSingleFacultyController,
+} from "../../controllers/v1/faculty";
 import { validateRequest } from "../../middleware/validate.middleware";
 import { facultyValidationSchema } from "../../validation/faculty.validation";
 import { authenticate, authorize } from "../../middleware/auth.middleware";
 
 const FacultyRouter = Router();
 const upload = multer();
+
+FacultyRouter.get("/", getAllFacultyController);
+FacultyRouter.get("/:id", getSingleFacultyController);
+
 FacultyRouter.post(
   "/",
   upload.none(),
