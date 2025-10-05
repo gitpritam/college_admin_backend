4f84908aae0efb577d6e79a9664fc392d13172c3
SayantikaDey2004 <sayantika.dey2004@gmail.com>
2025-10-05 10:41:19 +0530

    refactor: simplify CORS configuration to allow only localhost:5173

--- src/index.ts
+++ src/index.ts
@@ -24,8 +24,6 @@ app.use(cors(corsOptions));
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const corsOptions = { origin: ["http://localhost:5173"], credentials: true };
-app.use(cors(corsOptions));
 app.use(cookieParser());
 
 app.use("/api", MainRouter); //localhost:5000/api
