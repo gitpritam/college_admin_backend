d113bf55509d49a91c60be4652ff4a8ce62ba806
gitpritam <pritammajhi10@gmail.com>
2026-01-31 20:07:28 +0530

    fix: correct inquiry router method from get to patch for updateInquiryController

--- src/routes/v1/inquiry.router.ts
+++ src/routes/v1/inquiry.router.ts
@@ -1,19 +1,24 @@
- import { Router } from "express";
- import multer from "multer";
- import { authorize } from "../../middleware/auth.middleware";
- import { validateRequest } from "../../middleware/validate.middleware";
- import createInquiryController from "../../controllers/v1/inquiry/createInquiry.controller";
- import getAllInquiryController from "../../controllers/v1/inquiry/getAllInquriy.controller";
- import getSingleInquiryController from "../../controllers/v1/inquiry/getSIngleInquiry.controller";
- import updateInquiryController from "../../controllers/v1/inquiry/updateInquiry.controller";
+import { Router } from "express";
+import multer from "multer";
+import { authorize } from "../../middleware/auth.middleware";
+import { validateRequest } from "../../middleware/validate.middleware";
+import createInquiryController from "../../controllers/v1/inquiry/createInquiry.controller";
+import getAllInquiryController from "../../controllers/v1/inquiry/getAllInquriy.controller";
+import getSingleInquiryController from "../../controllers/v1/inquiry/getSIngleInquiry.controller";
+import updateInquiryController from "../../controllers/v1/inquiry/updateInquiry.controller";
 import { inquiryValidationSchema } from "../../validation/inquiry.validation";
 
- const inquiryRouter = Router();
- const upload = multer();
+const inquiryRouter = Router();
+const upload = multer();
 
-inquiryRouter.post("/",authorize(["admin", "faculty"]),validateRequest(inquiryValidationSchema),createInquiryController);
+inquiryRouter.post(
+  "/",
+  authorize(["admin", "faculty"]),
+  validateRequest(inquiryValidationSchema),
+  createInquiryController,
+);
 inquiryRouter.get("/", getAllInquiryController);
 inquiryRouter.get("/:id", getSingleInquiryController);
-inquiryRouter.get("/", updateInquiryController);
+inquiryRouter.patch("/", updateInquiryController);
 
 export default inquiryRouter;
